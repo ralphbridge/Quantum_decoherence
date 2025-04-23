b3efe8cda20997cc579843f3d3905a2b4de755ee
Raúl Puente <rpuente2@huskers.unl.edu>
2025-04-23 15:09:17 -0500

    Add files via upload
    Added simple snippet to analyze voltage data on top of count data to check whether the triggers work in sync or not

--- Voltagedata.m
+++ Voltagedata.m
@@ -1,26 +1,29 @@
-N=20;
+N=10; % Number of voltage measurements per sweeping step
+bw=2; % Bin width
 
-data=readmatrix("voltage_041625.xlsx");
+Vdata=readmatrix("voltage_042125.xlsx");
 
-t=data(:,1);
-V=data(:,2);
+t=Vdata(:,1);
+V=Vdata(:,2);
 
-plot(t,V,'o')
-title('Raw data')
-xlabel('Relative time t s','FontSize',15)
-ylabel('RFA Voltage V','FontSize',15)
-grid on
+% plot(t,V,'o')
+% title('Raw data')
+% xlabel('Relative time t s','FontSize',15)
+% ylabel('RFA Voltage V','FontSize',15)
+% grid on
 
-figure
-plot(t(941:1220),V(941:1220),'o')
-axis([min(t(941:1260))-5 max(t(941:1260)) -1005.4 -1005.3])
-grid on
+% figure
+% plot(t(941:1220),V(941:1220),'o')
+% % axis([min(t(941:1260))-5 max(t(941:1260)) -950.5 -1005.3])
+% grid on
 
-Vave=zeros(1,length(t)/N);
-sigma=zeros(1,length(t)/N);
+Vave=zeros(length(t)/N,1);
+tave=zeros(length(t)/N,1);
+sigma=zeros(length(t)/N,1);
 
 for i=1:length(Vave)
     Vave(i)=mean(V((i-1)*N+1:i*N));
+    tave(i)=mean(t((i-1)*N+1:i*N));
     tmp=0;
     for j=1:N
         tmp=tmp+(Vave(i)-V((i-1)*N+j))^2;
@@ -30,14 +33,38 @@ end
 
 measurement=1:length(t)/N;
 
-figure
-errorbar(measurement,Vave,sigma,'both','o')
-title('Averaged data')
-xlabel('Measurement number n','FontSize',15)
-ylabel('RFA Voltage V','FontSize',15)
-grid on
+% figure
+% errorbar(measurement,Vave,sigma,'both','o')
+% title('Averaged data')
+% xlabel('Measurement number n','FontSize',15)
+% ylabel('RFA Voltage V','FontSize',15)
+% grid on
 
-figure
-errorbar(measurement(960/N:1220/N),Vave(960/N:1220/N),sigma(960/N:1220/N),'both','o')
-axis([min(measurement(960/N:1220/N))-1 max(measurement(960/N:1220/N))+1 -1005.4 -1005.3])
-grid on
+% figure
+% errorbar(measurement(960/N:1220/N),Vave(960/N:1220/N),sigma(960/N:1220/N),'both','o')
+% % axis([min(measurement(960/N:1220/N))-1 max(measurement(960/N:1220/N))+1 -1005.4 -1005.3])
+% grid on
+
+Cdata=readmatrix("Trigger_test.txt");
+
+bin=Cdata(:,1)/bw;
+cts=Cdata(:,2);
+
+% figure
+% plot(bin,cts,'o')
+% grid on
+
+tfix=zeros(size(cts));
+Vfix=zeros(size(cts));
+
+tfix(1)=tave(1);
+j=0;
+for i=2:length(tave)
+    if (tave(i)-tave(i-1))<1.2*bw
+        tfix(i+j)=tave(i);
+    else
+        for j=1:(round(tave(i)-tave(i-1))/2)
+            tfix(i+j)=tave(i-1)+j*bw;
+        end
+    end
+end
