9a0ccf98d772ba92007bf499b9942a6d3729fac7
Raúl Puente <rpuente2@huskers.unl.edu>
2025-06-05 14:07:00 -0500

    Update Voltagedata.m
    Updated code that computes and plots Energy cutoffs from preliminary data acquired for AMO seminar (just before the e gun PS died again, a couple of days before Wayne visited us)

--- Voltagedata.m
+++ Voltagedata.m
@@ -1,13 +1,21 @@
-N=10; % Number of voltage measurements per sweeping step
-bw=2; % Bin width
-vrange=20-0;
-vstep=0.05;
-steps=vrange/vstep;
+ctsmatrix=zeros(16/0.05,8);
+Vmatrix=zeros(16/0.05,8);
+gap=[];
+Defl=zeros(2,8);
 
-Vdata=readmatrix("data/voltage_042425.xlsx");
+for idx=1:8
+    Vdata=readmatrix(['data/voltage_050525_',int2str(idx),'.xlsx']);
+    Cdata=readmatrix(['data/counts_050525_',int2str(idx),'.txt']);
 
-t=Vdata(:,1);
-V=Vdata(:,2);
+    N=Vdata(15,4); % Number of voltage measurements per sweeping step
+    bw=Vdata(11,4); % Bin width
+    vrange=Vdata(12,4);
+    vstep=Vdata(13,4);
+    steps=vrange/vstep;
+    pos=Vdata(4,4);
+
+    t=Vdata(:,1);
+    V=Vdata(:,2);
 
 % plot(t,V,'o')
 % title('Raw data')
@@ -20,124 +28,160 @@ V=Vdata(:,2);
 % % axis([min(t(941:1260))-5 max(t(941:1260)) -950.5 -1005.3])
 % grid on
 
-Vave=zeros(length(t)/N,1);
-tave=zeros(length(t)/N,1);
-sigma=zeros(length(t)/N,1);
+    Vave=zeros(length(t)/N,1);
+    tave=zeros(length(t)/N,1);
+    sigma=zeros(length(t)/N,1);
 
-for i=1:length(Vave)
-    Vave(i)=mean(V((i-1)*N+1:i*N));
-    tave(i)=mean(t((i-1)*N+1:i*N));
-    tmp=0;
-    for j=1:N
-        tmp=tmp+(Vave(i)-V((i-1)*N+j))^2;
+    for i=1:length(Vave)
+        Vave(i)=mean(V((i-1)*N+1:i*N));
+        tave(i)=mean(t((i-1)*N+1:i*N));
+        tmp=0;
+        for j=1:N
+            tmp=tmp+(Vave(i)-V((i-1)*N+j))^2;
+        end
+        sigma(i)=sqrt(tmp/(N-1));
     end
-    sigma(i)=sqrt(tmp/(N-1));
+
+    measurement=1:length(tave);
+
+    bin=Cdata(:,1)/bw;
+    cts=Cdata(:,2);
+
+    m=round(length(t)/N/steps);
+
+    extras=(length(cts)-steps*m)/m;
+
+    cfinal=zeros(steps,1);
+    Vfinal=zeros(steps,1);
+    sigmafinal=zeros(steps,1);
+
+    for i=1:steps
+        Vfinal(i)=Vave(i);
+        sigmafinal(i)=sigma(i);
+    end
+
+    for i=1:m
+        cfinal=cfinal+cts((i-1)*steps+(i-1)*extras+1:i*steps+(i-1)*extras);
+    end
+
+    % figure
+    % yyaxis left;
+    % plot(1:length(Vfinal),Vfinal,'o')
+    % title('Averaged data')
+    % xlabel('Measurement number n','FontSize',15)
+    % ylabel('RFA Voltage V','FontSize',15)
+    % title(['Gap position ',num2str(pos),' mm'])
+    % grid on
+    % 
+    % yyaxis right;
+    % plot(1:length(Vfinal),cfinal,'o')
+    % ylabel('Counts','FontSize',15)
+    % grid on
+
+    Vfinal=Vfinal';
+    Vfinal=abs(fliplr(Vfinal));
+    Vmatrix(:,idx)=Vfinal;
+
+    cfinal=cfinal';
+    cfinal=fliplr(cfinal);
+    ctsmatrix(:,idx)=cfinal;
+
+    gap=horzcat(gap,pos);
+    Defl(1,idx)=Vdata(8,4);
+    Defl(2,idx)=Vdata(9,4);
+end
+
+%% Fitting section
+% datatemp=ctsmatrix;
+data=ctsmatrix;
+
+%gap=[9.24,10,9.5,9.3,12.5,13,18,20.5];
+
+cutoff=zeros(size(gap));
+DeltaV_cutoff=zeros(size(gap));
+for i=1:length(gap)
+    V=Vmatrix(:,i);
+    vec1=[];
+    vec2=[];
+    V1=[];
+    V2=[];
+    for j=1:length(V)
+        if V(j)<(max(V)-min(V))/4+min(V)
+            V1=horzcat(V1,V(j));
+            vec1=horzcat(vec1,data(j,i));
+        end
+    end
+    P1=polyfit(V1,vec1,1);
+    if i==length(gap)
+        1;
+    end
+    for j=length(V):-1:1
+        if data(j,i)>0.1*(P1(1)*V(j)+P1(2)) && data(j,i)>0.05*max(data(:,i))
+            if data(j,i)<0.8*(P1(1)*V(j)+P1(2))
+                V2=horzcat(V(j),V2);
+                vec2=horzcat(data(j,i),vec2);
+            else
+                break
+            end
+        end
+    end
+    P2=polyfit(V2,vec2,1);
+
+    figure
+    subplot(2,1,1)
+    p=plot(V,data(:,i),'-o','color','black','LineWidth',1.5);
+    p.MarkerFaceColor=[0 0 0];
+    p.MarkerSize=3;
+    hold on
+    plot(V,P1(1)*V+P1(2),'color','blue','LineWidth',2)
+    plot(V,P2(1)*V+P2(2),'color','red','LineWidth',2)
+    txt=['\leftarrow Cutoff: ' num2str(cutoff(i)) ' V'];
+    t=text(cutoff(i),(-P2(1)*P1(2)+P1(1)*P2(2))/(P1(1)-P2(1))/2,txt);
+    t.FontSize = 14;
+    grid on
+    xlabel('$V_{RFA}$ (V)','Interpreter','latex','FontSize',16)
+    ylabel('$\# e^-$ (Counts/s)','Interpreter','latex','FontSize',16)
+    title(['Gap height ',num2str(round((gap(i)-8.57)*50/11.2)),' \mu m'])
+    %title(['Gap position ',num2str(gap(i)),' mm for DeflX=',num2str(Defl(1,i)),'V and DeflY=',int2str(Defl(2,i)),' V'])
+    axis([min(V) max(V) 0 1.2*max(data(:,i))])
+
+    %%%%%% Wayne's idea to understand bump
+    bumpC=zeros(length(V),1);
+
+    for j=1:length(V)
+        if data(j,i)-(P1(1)*V(j)+P1(2))>=0
+            bumpC(j)=data(j,i)-(P1(1)*V(j)+P1(2));
+        end
+    end
+
+    subplot(2,1,2)
+    plot(V,bumpC,'.')
+    grid on
+    %%%%%%
+
+    cutoff(i)=-P2(2)/P2(1);
+    DeltaV_cutoff(i)=(P2(1)*P1(2)-P1(1)*P2(2))/((P1(1)-P2(1))*P2(1));
 end
 
 % figure
-% errorbar(measurement,Vave,sigma,'both','o')
-% title('Averaged data')
-% xlabel('Measurement number n','FontSize',15)
-% ylabel('RFA Voltage V','FontSize',15)
+% p=plot((gap-8.57)*50/11.2,cutoff,'o');
+% p.MarkerFaceColor=[0 0 0];
+% p.MarkerSize=5;
+% hold on
+% for i=1:length(gap)
+%     txt=['\DeltaV: ' num2str(DeltaV_cutoff(i)) ' V'];
+%     t=text(gap(i)-1,cutoff(i)-0.05,txt);
+%     t.FontSize = 14;
+% end
+% xlabel('Gap height $z\ \mu m$','Interpreter','latex','FontSize',30)
+% ylabel('$V_{cutoff}\ V$','Interpreter','latex','FontSize',30)
+% xTickLocations=[0 5 10 15 20 25];
+% yTickLocations=[1000 1010 1020];
+% yticks=([1000 1010 1020]);
+% plot(gap,1017.7-(0.3*(1.5)^3)./gap)
+% set(gca,'XTick', xTickLocations);
+% set(gca,'YTick', yTickLocations);
+% axis([0 60 952 955])
 % grid on
-
-% figure
-% errorbar(measurement(960/N:1220/N),Vave(960/N:1220/N),sigma(960/N:1220/N),'both','o')
-% % axis([min(measurement(960/N:1220/N))-1 max(measurement(960/N:1220/N))+1 -1005.4 -1005.3])
-% grid on
-
-Cdata=readmatrix("data/Trigger_042425.txt");
-
-bin=Cdata(:,1)/bw;
-cts=Cdata(:,2);
-
-m=round(length(t)/N/steps);
-
-for i=2:length(tave)-m
-    if abs(tave(i)-tave(i-1))>2*bw
-        extras=round((tave(i)-tave(i-1)))/bw;
-        tave(i-1)=[];
-        Vave(i-1)=[];
-    end
-end
-
-measurement=1:length(tave);
-
-tfix=zeros(size(cts));
-Vfix=zeros(size(cts));
-
-% figure
-% subplot(1,2,1)
-% plot(measurement,Vave,'o')
-% grid on
-
-j=0;
-for i=1:m
-    Vfix((i-1)*steps+(i-1)*extras+1:i*steps+(i-1)*extras)=Vave((i-1)*steps+1:i*steps);
-    tfix((i-1)*steps+(i-1)*extras+1:i*steps+(i-1)*extras)=tave((i-1)*steps+1:i*steps);
-    if i<m
-        Vfix(i*steps+(i-1)*extras+1:i*steps+i*extras)=Vave(i*steps+1);
-        tfix(i*steps+(i-1)*extras+1:i*steps+i*extras)=tave(i*steps+1);
-    end
-end
-
-cts(find(tfix==0),:)=[];
-bin(find(tfix==0),:)=[];
-Vfix(find(tfix==0),:)=[];
-tfix(find(tfix==0),:)=[];
-measurementfix=1:length(tfix);
-
-bin=bin+1;
-
-Vfix=abs(Vfix);
-Vfix=Vfix-min(Vfix);
-
-figure
-yyaxis left;
-plot(measurementfix,Vfix,'o')
-title('Averaged data')
-xlabel('Measurement number n','FontSize',15)
-ylabel('RFA Voltage V','FontSize',15)
-grid on
-
-yyaxis right;
-plot(bin,cts,'o')
-ylabel('Counts','FontSize',15)
-grid on
-
-xline(400)
-xline(425)
-xline(825)
-xline(850)
-xline(1250)
-xline(1275)
-xline(1675)
-
-figure
-yyaxis left;
-plot(measurementfix(1:400),Vfix(1:400),'o')
-title('Averaged data')
-xlabel('Measurement number n','FontSize',15)
-ylabel('RFA Voltage V','FontSize',15)
-grid on
-
-yyaxis right;
-plot(bin(1:400),cts(1:400),'o')
-ylabel('Counts','FontSize',15)
-grid on
-
-hold on
-yyaxis right;
-plot(bin(1:400),cts(426:825),'bo')
-ylabel('Counts','FontSize',15)
-grid on
-
-yyaxis right;
-plot(bin(1:400),cts(851:1250),'mo')
-ylabel('Counts','FontSize',15)
-grid on
-
-yyaxis right;
-plot(bin(1:400),cts(1276:1675),'ko')
-ylabel('Counts','FontSize',15)
-grid on
+% yline(953.75)
+% yline(953.25)
